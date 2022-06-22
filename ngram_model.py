4d1804dd64a6893db8ae00bb95ccca57efef4acf
sean pauley <seanpatrickpauley@gmail.com>
2022-06-22 08:48:31 -0400

    fixed the ngram model!

--- ngram_model.py
+++ ngram_model.py
@@ -1,6 +1,6 @@
 #imports necessary packages
-from gensim.models import Phrases
 import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer
 
 #function for building the bigram model
 #takes a pandas series of texts (ex. df['text']) and creates a bigram model
@@ -8,20 +8,30 @@ import pandas as pd
 #text_series: pandas series containing the text
 #min_count: parameter for ngram mdoel; only ngrams that occur this many times or more will be included
 
-#based off code found at: https://radimrehurek.com/gensim/auto_examples/tutorials/run_lda.html
+#based off code found at: https://towardsdatascience.com/text-analysis-basics-in-python-443282942ec5
 
-def bigram_model(text_series, min_count):
+def ngram_model(text_series, min_count):
 
-    #builds the bigram model on the series
-    model = Phrases(text_series, min_count = min_count)
+    #builds the bigram (2) and trigram (3) model on the series
+    c_vec = CountVectorizer(ngram_range=(2,3))
+    ngrams = c_vec.fit_transform(text_series)
+    count_values = ngrams.toarray().sum(axis=0)
+    vocab = c_vec.vocabulary_
 
-    #adds the ngram to the document if it occurs more times than the min_count
+    #builds a dataframe of ngrams and frequencies
+    df_ngram = pd.DataFrame(sorted([(count_values[i],k) for k,i in vocab.items()], reverse=True)
+            ).rename(columns={0: 'frequency', 1:'ngram'})
+
+    #filters to include only those occuring more than min_count
+    df_ngram = df_ngram[df_ngram['frequency']>=min_count]
+
+    #adds the relevant ngram to the documents
     for idx in range(text_series.shape[0]):
 
-        #if the token contains an underscore (indicates bigram), adds to document
-        for token in model[text_series[idx]]:
-            if '_' in token:
-                    text_series[idx].append(token)
+        #if the document at the index contains the bigram, adds it to the document
+        for ngram in df_ngram['ngram']:
+            if ngram and ngram in text_series[idx]:
+                text_series[idx] = text_series[idx] + ' ' + '_'.join(ngram.split())
 
     return text_series
                 
--- run_model.py
+++ run_model.py
@@ -1,10 +1,15 @@
 #imports necessary packages and functions
 from data_preprocess import preprocess
-from ngram_model import bigram_model
+from ngram_model import ngram_model
 import pandas as pd
 
 #gets user input to define parameters
 df = pd.read_csv (str(input('input csv file name: '))+'.csv')
 col = str(input('input column name containing the text: '))
 
+#step one: run the preprocess function
 df1 = preprocess(df[col])
+
+#step two: get parameters and run ngram model
+min_count = str(input('input min_count for ngram model: '))
+df2 = ngram_model(df1, min_count)
